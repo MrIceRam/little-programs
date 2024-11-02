3aedd35423c3723dfbe5173a2ea77336d22d5d01
MrIceRam <skyfarliv@gmail.com>
2024-11-02 23:52:06 +0300

    maksimka

--- test 1.cpp
+++ test 1.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 
+int maks(int x){
+    if (x == 18){
+        return 1;
+    }
+    else{
+        return 2;
+    }
+}
+
+
 
 int main(){
     setlocale(LC_ALL, "ru");
-
-    std::cout<<2 + 3<< std::endl;
-    std::cout<< 90 - 10 + 20;
-    return 0;
+    int a;
+    std::cout << "age = ";
+    std::cin >> a;
+    if (a != 0){
+        std::cout << maks(a) << "\n";
+        main();
+    }
 }
