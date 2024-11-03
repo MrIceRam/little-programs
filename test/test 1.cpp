a32873a6dc5a80d204943be3f0efb67472e754d0
MrIceRam <skyfarliv@gmail.com>
2024-11-03 17:53:43 +0300

    17-53

---
+++ test/test 1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <ctime>
+#include <fstream>
+#include <string>
+
+int maks(int x,int b){
+    if (x == b){
+        return 1;
+    }
+    if (x == 0){
+        std::cout << "GG\n";
+        exit(0);        
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int main(){
+    setlocale(LC_ALL, "ru");
+    srand(time(0));
+    std::ofstream file;
+    file.open("random.txt");
+    std::string random;
+    random = file;
+
+    // int a;
+    // int b = 1 + rand() % 10;
+    // std::cout << "b = "<< b << "\n";
+    // std::cout << "age = ";
+    // std::cin >> a;
+    // std::cout << maks(a,b) << "\n";
+    // for (int i = 0; i < 10; i++){
+    //     int b = 1 + rand() % 20;
+    //     std::cout << i << " = " << b << "\n";
+    //     main();
+    // }
+}
---
+++ test/test 1.py
@@ -0,0 +1,8 @@
+import random
+
+RandomNum = random.randint(1,10)
+
+f = open('/Random number.txt','r')
+print(f)
+
+print("Random = ", RandomNum)
