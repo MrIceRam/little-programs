bc5539973f5228b47aa557b7a050fb70ee5b78bc
MrIceRam <skyfarliv@gmail.com>
2024-12-25 00:49:01 +0300

    num

--- PyImport/Python1.py
+++ PyImport/Python1.py
@@ -1,6 +1,11 @@
-from Python2 import lol
-while 1:
-    print("x = ")
-    a = int(input())
-    a = lol(a)
-    print(a)
+# file = open("PyImport/num.txt", "r")
+# content = file.read()
+# print(content)
+# print(int(content) + 1)
+# x = input()
+
+
+
+# file = open("example.txt", "w")
+# file.write("Hello, World!")
+# file.close()
