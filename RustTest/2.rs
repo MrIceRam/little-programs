c7eb88d9a9e2a11fe398467dfdf0283748670ecb
MrIceRam <skyfarliv@gmail.com>
2025-11-17 01:28:07 +0300

    add RustTest 2

--- RustTest/2.rs
+++ RustTest/2.rs
@@ -1,3 +1,10 @@
 fn main(){
-    
+    let x:i32=1;
+    let x2=demo(x);       
+    println!("{x2}")
 }
+fn demo(x:i32)->i32{ // -> значит тут вернется число с i32
+
+    println!("hi demo");
+    x+2
+} // end rust 2 6:01
