e7f0ecbecc6d39a83990d4243d7cfde593831f3b
MrIceRam <skyfarliv@gmail.com>
2025-11-22 01:10:38 +0300

    RustTest add 3.rs

--- RustTest/2.rs
+++ RustTest/2.rs
@@ -1,10 +1,22 @@
 fn main(){
-    let x:i32=1;
-    let x2=demo(x);       
-    println!("{x2}")
+    let mut x:i32=1;   // 2**32 int string bool float cahr
+    let x2 = demo(x);       
+    println!("{x2}");
+   loop{
+    println!("loop 6");
+    x = x + 1;
+    if x == 10{
+        break;
+    }
 }
+    let x3 =[10,20,30,40,50];
+    for i in x3 {
+        println!("{i}")
+    }
+}
+
 fn demo(x:i32)->i32{ // -> значит тут вернется число с i32
 
     println!("hi demo");
-    x+2
+    return x+2
 } // end rust 2 6:01
---
+++ RustTest/3.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let my_number = 15; // This is an i32
+    let single_reference = &my_number; //  This is a &i32
+    let double_reference = &single_reference; // This is a &&i32
+    let five_references = &&&&&my_number; // This is a &&&&&i32
+    print!("{my_number} {single_reference} {double_reference} {five_references}")
+}
