ab4fbcaa8470dcf4b70e2f358ff1f4bbfa977642
IceRam <skyfarliv@gmail.com>
2026-01-18 23:39:58 +0300

    std::ifstream file(

--- fileRandom/std-random.cpp
+++ fileRandom/std-random.cpp
@@ -13,4 +13,16 @@ int main(){
     std::ifstream read("RandomNUM.txt", std::ios::out);
     read >> i;
     std::cout << "c++ num: "<< i << "\n";
+    
+    // ПРАВИЛЬНЕЕ
+    // std::string line;
+    // std::ifstream file("input.txt");
+    // if (file.is_open()){
+    //     while (getline(file, line)){}
+    //         cout << line << "\n";
+    //         file.close();
+    // }
+    // else{
+    //     cout << "Unable to open file";
+    // return 0;}
 }
